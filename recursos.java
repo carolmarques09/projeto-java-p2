806138dd9c72315e8b0a54603cc7f29c4d16cf37
PedroFariasPaiva <164401307+PedroFariasPaiva@users.noreply.github.com>
2024-12-01 17:22:40 -0300

    Update recursos.java

--- recursos.java
+++ recursos.java
@@ -4,8 +4,8 @@ public interface recursos {
     void mover(int x, int y);
     void atacar(recursos target);
     void pegarRecursos(String resourceType);
-    void pegar();
-    void usar();
-    void soltar();
-    void descartar();
+    void pegar(String objeto);
+    void usar(String objeto);
+    void soltar(String objeto);
+    void descartar(String objeto);
 }
